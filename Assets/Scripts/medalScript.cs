afddd45b7526ab01541e26fc7dafe51a2efb61f8
unknown <Jorge.Carroz001@mymdc.net>
2024-02-22 13:50:16 -0500

    Achievments added to main menu with testing UI

---
+++ Assets/Scripts/medalScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class medalScript
+{
+    //Achievement for hidden elements:
+    public static bool medal1Hidden = false;
+    //Achievement for Completing in <10:
+    public static bool medal2Speed = false;
+}
